ba789144ef3bdbb4e23f2972e0718a02e3351474
NatalieBlack <natalievictoriablack@gmail.com>
2019-01-05 20:59:38 -0500

    Initial commit

---
+++ robot.js
@@ -0,0 +1,38 @@
+function newRobot(needs_repairs, foreign_model, vintage_model) {
+  return {
+    needs_repairs: needs_repairs,
+    foreign_model: foreign_model,
+    vintage_model: vintage_model,
+    todos: []
+  };
+}
+
+function station(robot) {
+  if (robot.needs_repairs && robot.foreign_model) {
+    return 1;
+  } else if (robot.needs_repairs && robot.vintage_model) {
+    return 2;
+  } else if (robot.needs_repairs) {
+    return 3;
+  } else {
+    return 4;
+  }
+}
+
+function prioritizeTasks(robot) {
+  if (robot.todos.length > 0) {
+    return Math.max(...robot.todos);
+  } else {
+    return -1;
+  }
+}
+
+function isWorkday(robot, today) {
+  return today !== robot.dayOff;
+}
+
+module.exports.newRobot = newRobot;
+module.exports.station = station;
+module.exports.isWorkday = isWorkday;
+module.exports.prioritizeTasks = prioritizeTasks;
+
